d452b2a1330a734bdd96b8a14c9f9160ffa01acc
Joseph Ollis <joe@Josephs-MacBook-Pro.local>
2023-05-31 22:18:26 -0500

    added dice images instead of textView.

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 /**
  * This activity allows the user to roll a dice and view the result
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+        rollDice()
 
     }
 
@@ -27,11 +28,22 @@ class MainActivity : AppCompatActivity() {
         // Create new Dice object with 6 sides and roll it
 
         val dice = Dice(6)
-
         val diceRoll = dice.roll()
-        // Update the screen with the dice roll
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+        val diceImage : ImageView = findViewById(R.id.imageView)
+
+
+        val drawableResource = when (diceRoll) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> {R.drawable.dice_6}
+
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage.contentDescription = diceRoll.toString()
     }
     /**
      * Roll the dice and update the screen with the result.
